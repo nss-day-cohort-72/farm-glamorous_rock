4d5e65544496d561afeb5c313aef1e6111ee52a1
Daniel Henderson <djhindu17@gmail.com>
2024-08-07 14:43:19 -0500

    created tractor.js and imported plantSeeds and usePlants into main.js from tractor.js and field.js respectfully

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,16 @@
-import { createPlan } from './plan.js'
-
-const yearlyPlan = createPlan();
-
 console.log("Welcome to the main module")
 
-console.log(yearlyPlan);
+import { createPlan } from './plan.js'
+import { plantSeeds } from './tractor.js';
+import { usePlants } from './field.js';
+
+
+const yearlyPlan = createPlan();
+console.log("Yearly Plan", yearlyPlan)
+
+plantSeeds(yearlyPlan);
+
+const plantsInField =usePlants();
+console.log("Plants in Field", plantsInField)
+
 
